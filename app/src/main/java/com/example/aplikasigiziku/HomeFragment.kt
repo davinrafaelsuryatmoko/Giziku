cd33832e188b363b1e50fe5b5cb376bdf347da0f
davinrafaelsuryatmoko <davinrafaelsuryatmoko@gmail.com>
2025-12-01 00:29:27 +0700

    membuat tampilan fragment_kal_gizi.xml ,membuat item_makanan.xml dengan gambar nasi_item.png untuk tampilan kal gizi dan memperbaiki HomeFragment.kt,

--- app/src/main/java/com/example/aplikasigiziku/HomeFragment.kt
+++ app/src/main/java/com/example/aplikasigiziku/HomeFragment.kt
@@ -5,8 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeFragment : Fragment() {
 
@@ -17,21 +19,23 @@ class HomeFragment : Fragment() {
 
         val view = inflater.inflate(R.layout.fragment_home, container, false)
 
-        val btnKalkulator = view.findViewById<Button>(R.id.btnKalkulator)
-        val btnVitamin = view.findViewById<Button>(R.id.btnVitamin)
-        val btnTips = view.findViewById<Button>(R.id.btnTips)
+        val btnKalkulator = view.findViewById<LinearLayout>(R.id.menuKalkulator)
+        val btnVitamin = view.findViewById<LinearLayout>(R.id.menuVitamin)
+        val btnTips = view.findViewById<LinearLayout>(R.id.menuTips)
+
+        val bottomNav = requireActivity().findViewById<BottomNavigationView>(R.id.nav_bottom)
 
         // ✅ Langsung ke DESTINATION, bukan ACTION
         btnKalkulator.setOnClickListener {
-            findNavController().navigate(R.id.nav_kalkulator)
+            bottomNav.selectedItemId = R.id.nav_kalkulator
         }
 
         btnVitamin.setOnClickListener {
-            findNavController().navigate(R.id.nav_vitamin)
+            bottomNav.selectedItemId = R.id.nav_vitamin
         }
 
         btnTips.setOnClickListener {
-            findNavController().navigate(R.id.nav_tips)
+            bottomNav.selectedItemId = R.id.nav_tips
         }
 
         return view
