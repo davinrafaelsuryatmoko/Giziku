99c934430e75c3e3e4d03945bfbc2a2d365f95c1
suqya <suqyarohmatin123@gmail.com>
2025-12-11 01:26:01 +0700

    membenahi halaman TipsFragment.kt

--- app/src/main/java/com/example/aplikasigiziku/TipsFragment.kt
+++ app/src/main/java/com/example/aplikasigiziku/TipsFragment.kt
@@ -4,7 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 
 class TipsFragment : Fragment() {
@@ -15,24 +16,26 @@ class TipsFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_tips, container, false)
 
-        val btn1 = view.findViewById<Button>(R.id.btnSelesai1)
-        val btn2 = view.findViewById<Button>(R.id.btnSelesai2)
+        val btn1 = view.findViewById<TextView>(R.id.btnSelesai1)
+        val btn2 = view.findViewById<TextView>(R.id.btnSelesai2)
 
-        setupToggleButton(btn1)
-        setupToggleButton(btn2)
+        setupToggle(btn1)
+        setupToggle(btn2)
 
         return view
     }
 
-    private fun setupToggleButton(button: Button) {
+    private fun setupToggle(button: TextView) {
         button.setOnClickListener {
-            val isSelected = !button.isSelected
-            button.isSelected = isSelected
+            val selected = !button.isSelected
+            button.isSelected = selected
 
-            if (isSelected) {
-                button.setTextColor(resources.getColor(android.R.color.white))
+            if (selected) {
+                button.setTextColor(ContextCompat.getColor(requireContext(), android.R.color.white))
+                button.setBackgroundResource(R.drawable.bg_button_tips_selected)
             } else {
-                button.setTextColor(resources.getColor(R.color.biru))
+                button.setTextColor(ContextCompat.getColor(requireContext(), R.color.biru))
+                button.setBackgroundResource(R.drawable.bg_button_tips_unselected)
             }
         }
     }
